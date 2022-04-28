4310a21c86f16f56036b65cf7c2d4637ae5b199b
Prajwal-YR <yr.prajwal@gmail.com>
2022-04-28 02:41:10 -0700

    Testing

---
+++ tests/helpers.test.ts
@@ -0,0 +1,38 @@
+import { importObject } from "./import-object.test";
+import { run as runner } from "../runner";
+import { parse } from "../parser";
+import { typeCheckProgram, TypeEnv } from "../typecheck";
+// Modify typeCheck to return a `Type` as we have specified below
+export function typeCheck(source: string): Type {
+  try {
+    var ast = parse(source)
+    console.log(JSON.stringify(ast, null, 2))
+    const ProgramEnv: TypeEnv = { vars: new Map(), funcs: new Map(), classes: new Map(), retType: "None", };
+    const typedAst = typeCheckProgram(ast, ProgramEnv);
+    return typedAst.stmts[typedAst.stmts.length - 1].a;
+  }
+  catch (err) {
+    err.message = "TYPE ERROR: " + err.message
+    throw err;
+  }
+}
+
+// Modify run to use `importObject` (imported above) to use for printing
+export async function run(source: string) {
+  const config = { importObject };
+  await runner(source, config);
+  return;
+}
+
+type Type =
+  | "int"
+  | "bool"
+  | "None"
+  | { tag: "object", class: string }
+
+export const NUM: Type = "int";
+export const BOOL: Type = "bool";
+export const NONE: Type = "None";
+export function CLASS(name: string): Type {
+  return { tag: "object", class: name }
+};
